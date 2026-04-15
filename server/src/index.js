88314b1f20372a9b195e325fe5ba0dadb68fb8e1
Vincent <vincent@steelandsoul.com>
2026-04-15 21:39:28 +0800

    Fix Express v5 wildcard route syntax

--- server/src/index.js
+++ server/src/index.js
@@ -30,7 +30,7 @@ app.use('/api/profile', profileRouter);
 if (isProd) {
   const clientDist = path.join(__dirname, '../../client/dist');
   app.use(express.static(clientDist));
-  app.get('*', (_req, res) => {
+  app.get('/{*path}', (_req, res) => {
     res.sendFile(path.join(clientDist, 'index.html'));
   });
 }
